7d39d33c80add594949618871bbf0bf39376a416
KGMalith <it18258622@my.sliit.lk>
2022-11-26 18:39:46 +0530

    text inputs, buttons components created

--- src/components/buttons/index.js
+++ src/components/buttons/index.js
@@ -1,68 +1,37 @@
 import React from 'react';
-import {TouchableOpacity, Text, Image} from 'react-native';
+import { TouchableOpacity, Text, Image } from 'react-native';
 import styles from './button-styles';
 
 const CustomButton = props => {
   return (
     <>
-      {props.isIconBtn ? (
-        <>
-          <TouchableOpacity
-            onPress={props.onPress}
-            style={
-              props.btnType == 'PRIMARY'
-                ? styles.primaryIconButton
-                : props.btnType == 'TRANSPARENT'
-                ? styles.transparentIconButton
-                : props.btnType == 'LIGHT'
-                ? styles.lightIconButton
-                : null
-            }
-            activeOpacity={props.opactity}>
-            <Image source={props.icon} style={styles.btnIcon} />
-          </TouchableOpacity>
-        </>
-      ) : (
-        <TouchableOpacity
-          onPress={props.onPress}
-          style={
-            props.btnType == 'PRIMARY'
-              ? styles.primaryButton
-              : props.btnType == 'SECONDARY'
-              ? styles.secondaryButton
-              : props.btnType == 'DARK'
-              ? styles.darkButton
-              : props.btnType == 'DARKLOGO'
-              ? styles.darkLogoButton
-              : props.btnType == 'SECONDARYLOGO'
-              ? styles.secondaryLogoButton
+      <TouchableOpacity
+        onPress={props.onPress}
+        style={
+          props.btnType == 'DARK'
+            ? styles.darkButton
+            : props.btnType == 'LIGHT'
+              ? styles.lightButton
               : null
-          }
-          activeOpacity={props.opactity}>
-          {props.frontIcon && (
-            <Image source={props.frontIcon} style={styles.frontIcon} />
-          )}
-          <Text
-            style={
-              props.btnType == 'PRIMARY'
-                ? styles.primaryButtonText
-                : props.btnType == 'SECONDARY'
-                ? styles.secondaryButtonText
-                : props.btnType == 'DARK'
-                ? styles.darkButtonText
-                : props.btnType == 'DARKLOGO'
-                ? styles.darkButtonText
-                : props.btnType == 'SECONDARYLOGO'
-                ? styles.secondaryButtonText
+        }
+        activeOpacity={props.opactity}>
+        {props.frontIcon && (
+          <Image source={props.frontIcon} style={styles.frontIcon} />
+        )}
+        <Text
+          style={
+            props.btnType == 'DARK'
+              ? styles.darkButtonText
+              : props.btnType == 'LIGHT'
+                ? styles.lightButtonText
                 : null
-            }>
-            {props.btnLabel}
-          </Text>
-          {props.rearIcon && (
-            <Image source={props.rearIcon} style={styles.rearIcon} />
-          )}
-        </TouchableOpacity>
-      )}
+          }>
+          {props.btnLabel}
+        </Text>
+        {props.rearIcon && (
+          <Image source={props.rearIcon} style={styles.rearIcon} />
+        )}
+      </TouchableOpacity>
     </>
   );
 };
@@ -71,7 +40,6 @@ export default CustomButton;
 
 // Set default props
 CustomButton.defaultProps = {
-  btnType: 'PRIMARY',
+  btnType: 'DARK',
   opactity: 0.85,
-  isIconBtn: false,
 };
--- src/screen/starter/index.js
+++ src/screen/starter/index.js
@@ -1,17 +1,24 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import { SafeAreaView, Image } from 'react-native';
-import { Layout, Text } from '@ui-kitten/components';
+import { SafeAreaView, Image, View } from 'react-native';
+import { Layout, Text, TopNavigation } from '@ui-kitten/components';
 import CustomButton from '../../components/buttons';
 import ASSETS from '../../assets/theme/assets';
 import styles from './starter-styles';
 import { Button } from '@ui-kitten/components';
 
 const Starter = ({ navigation }) => {
+  const renderTitle = props => (
+    <View style={styles.titleContainer}>
+      <Image style={styles.logo} source={ASSETS.app_logo} />
+    </View>
+  );
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.app_header} category="h1">
-        RENMO
-      </Text>
+      <Layout>
+        <TopNavigation alignment="center" title={renderTitle} />
+      </Layout>
       <Layout style={styles.detailsContainer}>
         <Layout style={styles.starterImageContainer}>
           <Image
@@ -20,22 +27,28 @@ const Starter = ({ navigation }) => {
           />
         </Layout>
         <Layout style={styles.textContainer}>
-          <Text style={styles.app_sub_title}>Renting Makes Easy!</Text>
-          <Text style={styles.app_sub_details}>
+          <Text category="h4" style={styles.app_sub_title}>
+            Renting Makes Easy!
+          </Text>
+          <Text category="s1" style={styles.app_sub_details}>
             Are you looking for rent House, Room, Vehicle or Tools? Renmo will
             help you on your journey!
           </Text>
         </Layout>
         <Layout style={styles.buttonContainer}>
-          <CustomButton
-            btnType="PRIMARY"
-            btnLabel="Log in"
-            onPress={() => navigation.navigate('Login')}
-          />
-
-          <Layout style={{ marginTop: 10 }}>
+          <Layout>
+            <Button
+              appearance="filled"
+              status="primary"
+              size="large"
+              style={styles.primaryBtnStyle}
+              onPress={() => navigation.navigate('Login')}>
+              Log in
+            </Button>
+          </Layout>
+          <Layout style={{ marginTop: 15 }}>
             <CustomButton
-              btnType="SECONDARY"
+              btnType="LIGHT"
               btnLabel="Sign up"
               onPress={() => navigation.navigate('Signup')}
             />
