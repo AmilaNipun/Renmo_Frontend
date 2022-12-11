ada20ad9ca90045950e1b188adc4d20314f49f92
KGMalith <it18258622@my.sliit.lk>
2022-12-11 19:41:30 +0530

    client booked item view created

--- src/screen/app screens/client/booked/index.js
+++ src/screen/app screens/client/booked/index.js
@@ -1,11 +1,38 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+/* eslint-disable prettier/prettier */
+import React, { useState } from 'react';
+import { FlatList, SafeAreaView, View } from 'react-native';
+import styles from './booked-styles';
+import { TopNavigation, Layout } from '@ui-kitten/components';
+import CustomInputs from '../../../../components/inputs';
+import { SearchIconEva } from '../../../../assets/theme/icons';
+import CustomItemCard from '../../../../components/Item card';
 
 const BookedScreen = ({ navigation }) => {
-    return (
-        <View>
-            <Text>BookedScreen</Text>
+    const [data, setData] = useState([]);
+
+    const renderTitle = props => (
+        <View style={styles.titleContainer}>
+            <CustomInputs
+                inputType="PRIMARY"
+                size="large"
+                placeholder="Search booked items"
+                iconLeft={SearchIconEva}
+            // onChangeText={}
+            />
         </View>
+    );
+
+    return (
+        <>
+            <SafeAreaView style={styles.mainContainer}>
+                <TopNavigation title={renderTitle} />
+                <View style={styles.divider} />
+                <Layout style={styles.bodyContainer}>
+                    <CustomItemCard />
+                    {/* <FlatList data={ } renderItem={ } /> */}
+                </Layout>
+            </SafeAreaView>
+        </>
     );
 };
 
