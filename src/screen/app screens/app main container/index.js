5b7ab2ee0f5d05e27a212e3eb8254687409eb40c
KGMalith <it18258622@my.sliit.lk>
2022-12-12 08:44:27 +0530

    updating admin view

--- src/screen/app screens/app main container/index.js
+++ src/screen/app screens/app main container/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { BottomNavigation, BottomNavigationTab } from '@ui-kitten/components';
 
@@ -17,26 +17,33 @@ import styles from './app-main-container-styles';
 
 const { Navigator, Screen } = createBottomTabNavigator();
 
-const BottomTabBar = ({ navigation, state }) => (
+const BottomTabBar = ({ userRole, navigation, state }) => (
   <BottomNavigation
     appearance="noIndicator"
     selectedIndex={state.index}
     style={styles.bottomNav}
     onSelect={index => navigation.navigate(state.routeNames[index])}>
-    <BottomNavigationTab title="Home" icon={HomeIconEva} />
-    <BottomNavigationTab title="Booked" icon={BookIconEva} />
-    <BottomNavigationTab title="Saved" icon={HeartIconEva} />
-    <BottomNavigationTab title="Profile" icon={PersonIconEva} />
+    {userRole === 0 ? (
+      <>
+        <BottomNavigationTab title="Home" icon={HomeIconEva} />
+        <BottomNavigationTab title="Booked" icon={BookIconEva} />
+        <BottomNavigationTab title="Saved" icon={HeartIconEva} />
+        <BottomNavigationTab title="Profile" icon={PersonIconEva} />
+      </>
+    ) : (
+      userRole === 1 && <></>
+    )}
   </BottomNavigation>
 );
 
 const AppMainContainer = () => {
+  const [userRole, setuserRole] = useState(0);
   return (
     <>
       <Navigator
         initialRouteName="Home"
         screenOptions={{ headerShown: false }}
-        tabBar={props => <BottomTabBar {...props} />}>
+        tabBar={props => <BottomTabBar userRole={userRole} {...props} />}>
         <Screen name="Home" component={HomeScreen} />
         <Screen name="Booked" component={BookedScreen} />
         <Screen name="Saved" component={SavedScreen} />
